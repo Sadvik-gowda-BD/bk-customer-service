3afed2203f06faedd2fa4eb0fb8cdfa891c49f3e
Sadvik <sadvikgowda3333@gmail.com>
2025-04-17 12:43:54 +0530

    added Hashicorp vault config

--- src/main/java/com/bank/customerservice/dto/CustomerDto.java
+++ src/main/java/com/bank/customerservice/dto/CustomerDto.java
@@ -1,7 +1,11 @@
 package com.bank.customerservice.dto;
 
+import com.bank.customerservice.annotation.CustomAgeCheck;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +19,23 @@ import lombok.NoArgsConstructor;
 public class CustomerDto {
     private int userId;
     @NotBlank(message = "User name should no be empty")
+    @Size(min = 2, max = 20)
     private String name;
     private char gender;
+    //@Email
+    @Email(
+            regexp = "\"[a-z0-9._%+-]+@[a-z0-9.-]+\\\\.[a-z]{2,3}\"",
+            flags = Pattern.Flag.CASE_INSENSITIVE,
+            message = "Inavlid email address"
+    )
     private String emailId;
+    @CustomAgeCheck(minAge = 10, maxAge = 30, message = "age should be between 10 and 20")
     private int age;
+    @Pattern(
+            regexp = "^[a-zA-Z]{2}\\d{2}[eEnN]$",
+            message = "User code must be 5 characters long: 2 letters, 2 numbers, and ends with E or N"
+    )
+    private String userCode;
+    private String number;
     private AddressDto address;
 }
